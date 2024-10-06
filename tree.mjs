9b5f2d5f948bd809c95a32b746dcedc6cf21b365
Hayyuu <hayatahimed518@gmail.com>
2024-10-06 20:01:29 +0300

    Add prettyPrint function for easy visualization of nodes

--- tree.mjs
+++ tree.mjs
@@ -20,8 +20,23 @@ class Tree{
     feedData(){
         return this.buildTree(this.arr,0,this.arr.length-1);
     }
+    
 }
 
-let data=[1,2,3,4,5,6];
+let data=[1,2,3,4,5,6,7,8,9];
 let tree=new Tree(data);
-console.log(tree.feedData());
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.left !== null) {
+      prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+  };
+
+prettyPrint(tree.feedData());
